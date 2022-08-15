00257f32c0965db23d4dd0720eeb2060e482ea70
Moin105 <moinlatif105@gmail.com>
2022-08-15 14:41:33 +0500

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d584878fadec54bf"; }
+/******/ 		__webpack_require__.h = function() { return "a76e3503eefd9046"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
