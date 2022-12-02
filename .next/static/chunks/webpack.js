0ba6525d989f1d8ac83492bda88341d405c63c06
Moin105 <moinlatif105@gmail.com>
2022-12-02 19:28:21 +0500

    sata

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "03ba31779cef0902"; }
+/******/ 		__webpack_require__.h = function() { return "24ea94d65c569389"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/header.js
+++ components/header.js
@@ -100,7 +100,7 @@ function Header() {
               About
             </Link>
           </motion.li>
-          <Link  href="/#contact" smooth="true"><li className={styles.btn}>Let&apos;s Talk</li></Link>
+          <Link  href="/Hire" smooth="true"><li className={styles.btn}>Let&apos;s Talk</li></Link>
         </ul>
         <span onClick={navHandle} className={styles.navbar}>
           <figure className={styles.ham}>
