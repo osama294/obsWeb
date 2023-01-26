dcd1b0d7b588dd1a0b18ded9a1a658b12518dc6a
moin105 <moinlatif105@gmail.com>
2023-01-26 15:45:06 +0500

    tasweer

--- components/contact.js
+++ components/contact.js
@@ -141,7 +141,7 @@ function Contact() {
                       transition: { duration: 0.1 },
                       boxShadow: `rgba(100, 100, 111, 0.2) 0px 7px 29px 0px`,
                     }}>
-                    Let's talk
+                    Let&apos;s talk
                   </motion.button>
                 </div>
               </div>
